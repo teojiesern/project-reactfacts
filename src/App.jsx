59e86e51876f70ce8078cf5a7897065137f2c82c
teojiesern <teojiesern42@gmail.com>
2023-04-21 17:27:04 +0800

    added initial navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,10 @@
 import React from "react"
-import Navbar
+import Navbar from "./assets/components/Navbar"
+
+export default function App(){
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,5 +1,16 @@
 import React from "react"
+import { FaReact } from "react-icons/fa";
 
 export default function Navbar(){
-    
+    return (
+        <div className="nav">
+            <div className="title">
+                <FaReact />
+                <h1>ReactFacts</h1>
+            </div>
+            <div className="toggle">
+                <h2>hola</h2>
+            </div>
+        </div>
+    )
 }
